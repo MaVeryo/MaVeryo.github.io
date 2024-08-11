495cf8797bd548ce993fd5a9e1610918d7da4805
Mahit Verma <mahitverma@gmail.com>
2024-08-11 11:30:06 -0400

    Updating using Aceternity UI
    Updated packages, and added a layout demo going to modify to my own liking but just wanting to make sure it works

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Navigation from "../components/Navigation"
+import { LayoutGridDemo } from "../components/LayoutGridDemo";
 export default function HomePage() {
   return (
     <div >
       <Navigation />
+      <LayoutGridDemo/>
     </div>
   );
 }
