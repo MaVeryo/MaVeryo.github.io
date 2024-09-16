4fe48662cf7e983bd781f71bcb6efe70a14f5f48
Mahit Verma <mahitverma@gmail.com>
2024-09-16 02:01:37 -0400

    Started integration of 3d Objects
    Started to integrate 3d objects into my website for more of an interative feel

---
+++ src/components/Mac.tsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Mac() {
+  return (
+    <Spline scene="https://prod.spline.design/2rhPRT2ey8b3yd6L/scene.splinecode" />
+  );
+}
