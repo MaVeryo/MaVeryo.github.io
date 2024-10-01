2a92fada60c10330b2f38ad61df235d7139da160
Mahit Verma <mahitverma@gmail.com>
2024-09-30 22:37:07 -0400

    added deploy submodule

--- dist/src/components/Mac.tsx
+++
@@ -1,7 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function Mac() {
-  return (
-    <Spline scene="https://prod.spline.design/2rhPRT2ey8b3yd6L/scene.splinecode" />
-  );
-}
